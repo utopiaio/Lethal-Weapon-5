e4ab1e965cbfe9ab1305a420a61e3854edc979e0
Moe Szyslak <moe.duffdude@gmail.com>
2017-03-30 09:52:25 +0300

    following the mp4 link for SD video
    TODO/Q:
    - following the hls link is causing a file dump rather that video link

--- index.js
+++ index.js
@@ -71,13 +71,13 @@ module.exports = (title, yearStart = YS, yearEnd = YE) => new Promise(async (res
 
     const videoClips = JSON.parse($.html().match(/var videoClips = (\[.+\]);/)[1]);
     // eslint-disable-next-line
-    const videosDirectURL = await axios.all(videoClips.map(videoClip => videoClipPromise(videoClip.urls.hls)));
+    const videosDirectURL = await axios.all(videoClips.map(videoClip => videoClipPromise(videoClip.urls.mp4)));
 
     // this is the part where you fix your face
     // Yes, I'm going to be mutating [enhancing] `videoClips` with `directHls` links
     videosDirectURL.forEach((directVideo, index) => {
       // eslint-disable-next-line
-      videoClips[index].urls.directHls = directVideo.status === 200 ? directVideo.request._options.href : null;
+      videoClips[index].urls.directMp4 = directVideo.status === 200 ? directVideo.request._options.href : null;
     });
 
     Object.assign(movie411, { trailers: videoClips });
